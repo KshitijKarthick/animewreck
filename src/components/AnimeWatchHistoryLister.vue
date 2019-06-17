c3b2e642f09ad403cf6b3f05b14e4b333f2795e0
Kshitij Karthick <kshitij.karthick@gmail.com>
2019-06-17 22:46:55 +0200

    Update AnimeWatchHistoryLister to utilise data table component

--- src/components/AnimeWatchHistoryLister.vue
+++ src/components/AnimeWatchHistoryLister.vue
@@ -4,30 +4,51 @@
       <h2 class="headline mb-3">Past History</h2>
     </v-flex>
     <v-flex>
-      <v-list>
-        <template>
-          <v-list-tile
-            v-for="(animeRating, index) in userWatchHistory"
-            :key="index"
-          >
-            <v-list-tile-content>
-              <v-list-tile-title
-                v-text="animeRating.title"
-              ></v-list-tile-title>
-            </v-list-tile-content>
-            <v-list-tile-content>
-              <v-list-tile-title
-                v-text="animeRating.rating"
-              ></v-list-tile-title>
-            </v-list-tile-content>
-            <v-list-tile-action>
-              <v-btn @click="removeAnimeRating(index)">
-                <v-icon>delete</v-icon>
-              </v-btn>
-            </v-list-tile-action>
-          </v-list-tile>
+      <v-data-table
+        :headers="headers"
+        :items="userWatchHistory"
+        v-if="userWatchHistory.length > 0"
+      >
+        <template v-slot:items="props">
+          <td class="text-xs-left">{{ props.item.title }}</td>
+          <td class="text-xs-left">
+            <v-edit-dialog
+              :return-value.sync="props.item.rating"
+              lazy
+              large
+              persistant
+              @save="saveEditBox"
+              @cancel="cancelEditBox"
+              @open="openEditBox"
+              @close="closeEditBox"
+            >
+              {{ props.item.rating }}
+              <template v-slot:input>
+                <div class="mt-3 title">Update Rating</div>
+              </template>
+              <template v-slot:input>
+                <v-text-field
+                  label="Rating between 1 to 10"
+                  single-line
+                  v-model="props.item.rating"
+                  type="number"
+                  min="1"
+                  max="10"
+                  required
+                  autofocus
+                  :rules="[rules.required, rules.inputRange]"
+                >
+                </v-text-field>
+              </template>
+            </v-edit-dialog>
+          </td>
+          <td>
+            <v-icon small class="mr-2" @click="removeAnimeRating(props.item)">
+              delete
+            </v-icon>
+          </td>
         </template>
-      </v-list>
+      </v-data-table>
     </v-flex>
   </div>
 </template>
@@ -45,9 +66,43 @@ export default {
       required: true
     }
   },
+  data: () => ({
+    headers: [
+      { text: "Anime", value: "title" },
+      { text: "Rating", value: "rating" }
+    ],
+    snack: false,
+    snackColor: "",
+    snackText: "",
+    rules: {
+      required: value => !!value || "Required.",
+      inputRange: value =>
+        (value > 0 && value <= 10) ||
+        "Rating should be between 0 and 10 [0, 10]"
+    }
+  }),
   methods: {
     removeAnimeRating: function(index) {
       this.userWatchHistory.splice(index, 1);
+    },
+    saveEditBox(value) {
+      console.log(value);
+      this.snack = true;
+      this.snackColor = "success";
+      this.snackText = "Data saved";
+    },
+    cancelEditBox() {
+      this.snack = true;
+      this.snackColor = "error";
+      this.snackText = "Canceled";
+    },
+    openEditBox() {
+      this.snack = true;
+      this.snackColor = "info";
+      this.snackText = "Dialog opened";
+    },
+    closeEditBox() {
+      console.log("Dialog closed");
     }
   }
 };
