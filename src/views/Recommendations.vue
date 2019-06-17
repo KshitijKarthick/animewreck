10628b977dbb3cb47778a32299235cba2fd2ebfe
Kshitij Karthick <kshitij.karthick@gmail.com>
2019-06-17 11:11:05 +0200

    Add view to list anime recommendations

---
+++ src/views/Recommendations.vue
@@ -0,0 +1,33 @@
+<template>
+  <AnimeRecommendationLister :recommendations="recommendations">
+  </AnimeRecommendationLister>
+</template>
+
+<script>
+import AnimeRecommendationLister from "../components/AnimeRecommendationLister";
+
+export default {
+  props: {
+    recommendationsArray: {
+      type: Array,
+      default: () => []
+    },
+    recommendationsJSON: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    recommendations: function() {
+      if (this.recommendationsJSON) {
+        return JSON.parse(this.recommendationsJSON);
+      } else {
+        return this.recommendationsArray;
+      }
+    }
+  },
+  components: {
+    AnimeRecommendationLister
+  }
+};
+</script>
