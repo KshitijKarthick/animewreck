61fc508e89c4063438b026191442a671f60a09d7
Kshitij Karthick <kshitij.karthick@gmail.com>
2019-06-17 11:08:18 +0200

    Add vue DOM mount code for the root component to be loaded

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app dark>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>AnimeWreck</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/KshitijKarthick/animewreck"
+        target="_blank"
+      >
+        <span class="mr-2">Personalised Recommendation Engine</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
