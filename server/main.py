18b214cb3763800204c538d2e42662b92c69d330
Kshitij Karthick <kshitij.karthick@gmail.com>
2019-06-18 08:53:18 +0200

    Ensure server allows client side router to manage its own routes

--- server/main.py
+++ server/main.py
@@ -2,12 +2,13 @@ import json
 import logging
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
-from starlette.responses import FileResponse
+from starlette.responses import RedirectResponse, FileResponse
 from starlette.middleware.cors import CORSMiddleware
 from server.model import generate_recommendations
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="dist", html=True), name="static")
+app.mount("/static/js", StaticFiles(directory="dist/js", html=True), name="static_js")
+app.mount("/static/css", StaticFiles(directory="dist/css", html=True), name="static_css")
 app.add_middleware(CORSMiddleware, allow_origins=['*'])
 
 with open('model_resources/cosine_nary_pairwise_distances_top50.json', 'r') as fp:
@@ -15,6 +16,23 @@ with open('model_resources/cosine_nary_pairwise_distances_top50.json', 'r') as f
 
 with open('model_resources/anime_info.json', 'r') as fp:
     ANIME_INFO = json.load(fp)
+
+@app.exception_handler(404)
+def validation_exception_handler(request, exc):
+    logging.info('Redirecting')
+    return FileResponse('dist/index.html')
+
+@app.get("/static")
+def render_homepage():
+    return FileResponse('dist/index.html')
+
+@app.get("/static/index.html")
+def render_homepage_abs():
+    return FileResponse('dist/index.html')
+
+@app.get("/static/favicon.ico")
+def render_ico():
+    return FileResponse('dist/favicon.ico')
 
 @app.get("/api/status")
 def render_homepage():
@@ -43,12 +61,5 @@ def recommendations(watch_history, topn=50):
             'recommendation_rating': 'rating',
             'target_anime_monotonic_id': 'id'
         }).to_dict(orient='record')
-    # return [
-    #     { 'title': 'a', 'rating': 2, 'id': 1 },
-    #     { 'title': 'b', 'rating': 3, 'id': 2 },
-    #     { 'title': 'c', 'rating': 4, 'id': 3 },
-    #     { 'title': 'd', 'rating': 5, 'id': 4 },
-    #     { 'title': 'e', 'rating': 6, 'id': 5 }
-    # ]
 
 logging.info('All application resources are loaded')
